97f61295f8fe0c4b5a1cff595c0cc4d0858a108a
Monika <monikazuwala@gmail.com>
2019-07-23 10:08:06 +0200

    Added joke.

--- gittie.py
+++ gittie.py
@@ -2,4 +2,10 @@ def introduction():
     print("Hello, I'm Gittie!")
 
 def joke():
-    print("What is the longest word in the English language? SMILES: there is a mile between the first and last letters!")
+    print("What is the longest word in the English language?\n SMILES: there is a mile between the first and last letters!")
+
+def main():
+    introduction()
+    joke()
+
+main()
