90213f1ba565d73bfec9dbb1845b4dfcf742e54a
pachi8 <pachi@poczta.onet.pl>
2019-07-23 10:58:18 +0200

    added random print

--- gittie.py
+++ gittie.py
@@ -4,13 +4,14 @@ def introduction():
 def add(a, b):
     print(int(a) + int(b))
 
-add(input("Write first number: "), input("Write second number: "))
-
 def joke():
     print("What is the longest word in the English language?\n SMILES: there is a mile between the first and last letters!")
 
 def main():
     introduction()
     joke()
+    add(input("Write first number: "), input("Write second number: "))
 
 main()
+
+print("Haha Barrrack Obama is the president again")
